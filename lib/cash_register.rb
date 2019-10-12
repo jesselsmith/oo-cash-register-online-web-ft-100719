1ec9dc79fc4c6fee1bde8d2e4f94298da2190c78
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-12 00:44:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
     self.items = []
     self
   end
-  
+
   def add_item(title, price, quantity = 1)
     self.total = ( price * quantity ) + self.total
     self.items << title
